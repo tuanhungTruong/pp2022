a35a8262149490ea8a666702deb8c3f660329977
tuanhungTruong <tuanhung.truong2709@gmail.com>
2022-04-13 20:38:38 +0700

    lab7@

--- 7.shell.py
+++ 7.shell.py
@@ -10,5 +10,22 @@ while True:
             print("bash: cd: No such file or directory")
     elif command == 'exit':
         break
+    elif command == 'ls -la':
+        while True:
+            try:
+                if os.path.isfile("file.txt") == False:
+                    open('file.txt', 'a').close()
+                file = open('file txt', 'a')
+                get_input = input(">>")
+                file.write(get_input + "\n")
+                print("Do you wish to continue ? yes or no \n")
+                command1 = input(">>>")
+                if command1 == "no":
+                    break
+                if command1 == "yes":
+                    print("\n")              
+            except IOError:
+                print("Something went wrong")
+
     else:
         os.system(command)
